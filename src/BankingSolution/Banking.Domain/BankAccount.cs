46151a6337a579862f14f55a42febcab3cefb7bd
Emily Ireland <emilyjireland49@gmail.com>
2025-09-09 20:50:50 +0000

    test driven dev, bank account cs, done w d2

---
+++ src/BankingSolution/Banking.Domain/BankAccount.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Banking.Domain;
+
+public class BankAccount
+{
+    public decimal GetBalance()
+    {
+        return 5000M; // hard coded for right now to pass test
+    }
+}
---
+++ src/BankingSolution/Banking.tests/NewAccount/HaveCorrectBalance.cs
@@ -0,0 +1,21 @@
+﻿
+using Banking.Domain;
+
+namespace Banking.tests.NewAccount;
+public class HaveCorrectBalance
+{
+    [Fact]
+    public void CorrectBalanceForNewAccounts()
+    {
+        // WTCYWHYH - WRITE THE CODE YOU WISH YOU HAD
+        // Given I have a new bank account
+        var account = new BankAccount();
+
+        // When I ask that account for the balance
+        decimal balance = account.GetBalance();
+
+        // Then it should be...
+
+        Assert.Equal(5000M, balance);
+    }
+}
