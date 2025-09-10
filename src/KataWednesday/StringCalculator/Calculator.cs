31732e9e397cd7060e6a374d2c0361bf12e3b36b
Emily Ireland <emilyjireland49@gmail.com>
2025-09-10 20:53:06 +0000

    helpful c# .NEt syntax comments

--- src/KataWednesday/StringCalculator/Calculator.cs
+++ src/KataWednesday/StringCalculator/Calculator.cs
@@ -24,3 +24,25 @@ public class Calculator
         return sum;
     }
 }
+
+// int --> System.Int32
+// string --> System.String
+
+// cant use var outside of a method, it's only for local variables
+
+// data type ex: string[] to declare array, can just use "var" -> interpreted by 
+// the compiler as being used as an array
+// .Select(int.Parse) --> like 'map'
+
+
+// alternative implementation:
+//return numbers
+//    .Split(',', '\n') --> ["1", "2", "3"]
+//    .Select(int.Parse) --> // [1, 2, 3] , so maps strings to ints 
+//    .Sum();
+
+// generics provide "parametric polymorphism"
+// var numbers = new List<int> {1, 2, 3, 4, 5, 6, 7, 8, 9}
+// var onlyTheEvens = numbers.Where(n => n % 2 == 0);
+// numbers[0] = 8; // lazy evaluation, "homoiconicity"
+// Assert.Equal(4, onlyTheEvents.Count());
