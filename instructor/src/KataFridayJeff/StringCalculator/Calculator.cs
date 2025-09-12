234e1d04c19cd48d6362e319265418964ff0bb05
Emily Ireland <emilyjireland49@gmail.com>
2025-09-12 15:48:55 +0000

    Jeff kata solution

---
+++ instructor/src/KataFridayJeff/StringCalculator/Calculator.cs
@@ -0,0 +1,41 @@
+﻿
+using StringCalculator.Helpers;
+
+public class Calculator
+{
+    public int Add(string numbers)
+    {
+        // Fewest Elements - variables, loops, and if statements.
+        // Variables - names for things that VARY - that change.
+        // constant - an immutable - never changes.
+        var delimeters = new List<char> { ',', '\n' };
+        if (numbers.IsEmptyString()) { return 0; }
+
+       
+       
+        if (numbers.HasACustomDelimeter())
+        {
+            delimeters.Add(numbers[2]);
+            numbers = numbers[4..]; // reassigning to a variable.
+        }
+        var results = numbers.Split(delimeters.ToArray()).Select(int.Parse);
+        if (results.Any(n => n < 0))
+        {
+            throw new NegativeNumbersNotAllowedException(string.Join(", ", results.Where(n => n < 0)));
+        }
+        return results
+        .Where(n => n <= 1000)
+        .Sum();
+    }
+
+   
+
+   
+}
+
+public class NegativeNumbersNotAllowedException : Exception
+{
+    public NegativeNumbersNotAllowedException(string message) : base(message) { }
+}
+
+
---
+++ instructor/src/KataFridayJeff/StringCalculator/CalculatorTests.cs
@@ -0,0 +1,92 @@
+﻿
+namespace StringCalculator;
+
+public class CalculatorTests
+{
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        var calculator = new Calculator();
+
+        var result = calculator.Add("");
+
+        Assert.Equal(0, result);
+    }
+
+    [Theory]
+    [InlineData("1", 1)]
+    [InlineData("2", 2)]
+    [InlineData("319", 319)]
+    public void SingleNumberReturnsValue(string input, int expected)
+    {
+        var calculator = new Calculator();
+        var result = calculator.Add(input);
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("1,2", 3)]
+    [InlineData("10,20", 30)]
+    [InlineData("100,250", 350)]
+    public void TwoNumbersCommaDelimitedReturnsSum(string input, int expected)
+    {
+        var calculator = new Calculator();
+        var result = calculator.Add(input);
+        Assert.Equal(expected, result);
+    }
+    [Theory]
+    [InlineData("1,2", 3)]
+    [InlineData("10,20,3,40", 73)]
+    public void ArbitraryLength(string input, int expected)
+    {
+        var calculator = new Calculator();
+        var result = calculator.Add(input);
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("1\n2", 3)]
+    [InlineData("10,20\n3,40", 73)]
+    public void MixedDelimeters(string input, int expected)
+    {
+        var calculator = new Calculator();
+        var result = calculator.Add(input);
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("//#\n1#2", 3)]
+    [InlineData("//#\n1#2,3", 6)]
+    [InlineData("//;\n10;20;3;40", 73)]
+    [InlineData("//a\n1a2a3", 6)]
+    [InlineData("//z\n10z20,3\n40", 73)]
+
+    public void CustomDelimeters(string input, int expected)
+    {
+        var calculator = new Calculator();
+        var result = calculator.Add(input);
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("1,-2", "-2")]
+    [InlineData("//;\n10;-20;3;-40", "-20, -40")]
+    public void ThrowsOnNegativeNumbers(string input, string negatives)
+    {
+        var calculator = new Calculator();
+        var exception = Assert.Throws<NegativeNumbersNotAllowedException>(() => calculator.Add(input));
+        Assert.Equal(negatives, exception.Message);
+    }
+
+    [Theory]
+    [InlineData("1001", 0)]
+    [InlineData("2,1001", 2)]
+    [InlineData("1000,1001", 1000)]
+    [InlineData("//;\n1000;1001;2", 1002)]
+    public void NumbersGreaterThan1000AreIgnored(string input, int expected)
+    {
+        var calculator = new Calculator();
+        var result = calculator.Add(input);
+        Assert.Equal(expected, result);
+    }
+}
